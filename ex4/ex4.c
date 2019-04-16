51fee7df42b2b079dafd3bb15e3f61311b3a83d2
Maollo <43157476+Maollo@users.noreply.github.com>
2019-04-16 20:08:09 -0300

    Create ex4.c
    finish

---
+++ ex4/ex4.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void inicializaVet(int *vet,int tam);
+int funcPos(int valor,int tam);
+void guardavetor(int *vetVal, int *vetqtd,int num, int tam);
+void SelectionSort(int *vetVal,int *vetQtd,int tam);
+int contaElementos(int *vetQtd,int tam);
+void saida(int *vetQtd,int *vetVal,int tam);
+
+
+
+int main(int argc, char const *argv[]) {
+  FILE *arq;
+  int tam, num, *vetnumeros, *vetqtd, a, b;
+  arq = fopen("entrada4.txt", "r");
+  fscanf(arq,"%d",&a);
+  fscanf(arq,"%d",&b);
+  tam = (b-a+1);
+  vetnumeros = (int*) malloc (sizeof(int) * tam);
+  vetqtd = (int*) malloc (sizeof(int) * tam);
+  inicializaVet(vetnumeros, tam);
+  inicializaVet(vetqtd, tam);
+  while(!feof(arq)){
+    fscanf(arq,"%d",&num);
+    guardavetor(vetnumeros, vetqtd, num, tam);
+  }
+
+  saida(vetqtd,vetnumeros,tam);
+
+	fclose(arq);
+  return 0;
+}
+
+
+
+void inicializaVet(int vet[],int tam){
+    int i;
+    for(i=0; i < tam; i++){
+        vet[i] = 0;
+    }
+}
+
+
+void guardavetor(int *vetVal, int *vetQtd,int num, int tam){
+  int pos;
+  pos = funcPos(num, tam);
+  vetVal[pos] = num;
+  vetQtd[pos]++;
+}
+
+
+int funcPos(int valor,int tam){
+    int indice;
+    indice = valor%tam;
+    return indice;
+}
+
+void SelectionSort(int *vetQtd,int *vetVal,int tam){
+    int i,j,aux,aux1, menor;
+    for (i = 0; i < tam-1; i++){
+        menor = i;
+        for (j = i+1; j < tam; j++){
+            if (vetQtd[j] > vetQtd[menor]){
+                menor = j;
+            }
+        }
+        aux1 = vetVal[i];
+        aux = vetQtd[i];
+        vetVal[i] = vetVal[menor];
+        vetQtd[i] = vetQtd[menor];
+        vetQtd[menor] = aux;
+        vetVal[menor] = aux1;
+	}
+}
+
+void saida(int *vetQtd,int *vetVal,int tam){
+	int i,n;
+	FILE *fp;
+
+	n = contaElementos(vetQtd,tam);
+
+	fp = fopen("saida4.txt","w");
+	if( fp == NULL){
+		puts("Erro na abertura do arquivo");
+	}
+	SelectionSort(vetQtd,vetVal,tam);
+
+	for( i = 0; i < n; i++){
+		if(i == 0){
+			fprintf(fp," %d (%dx)",vetVal[i],vetQtd[i]);
+		} else if( i != n-1){
+			fprintf(fp, " --> %d (%dx)",vetVal[i],vetQtd[i]);
+		}else{
+			fprintf(fp," --> %d (%dx)\n",vetVal[i],vetQtd[i]);
+		}
+
+	}
+	fclose(fp);
+	free(vetVal);
+	free(vetQtd);
+}
+
+int contaElementos(int *vetQtd,int tam){
+	int i,cont;
+	cont = 0;
+	for(i=0; i < tam; i++){
+		if(vetQtd[i] != 0){
+			cont++;
+		}
+	}
+	return cont;
+}
